970b8f26ccd0a2f97ec9cea98e3a6d44c15d81a4
dorian <sdorian07@gmail.com>
2025-09-21 17:49:11 -0600

    CompilaciÃ³n automÃ¡tica - Nueva versiÃ³n

--- src/Prueba1.cpp
+++ src/Prueba1.cpp
@@ -1,7 +1,20 @@
+//#include <iostream>
+//using namespace std;
+
+//int main() {
+  //  cout << "Hola mundo desde C++ (Version 1)" << endl;
+    //return 0;
+//}
+
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
-    cout << "Hola mundo desde C++ (Version 1)" << endl;
+    string nombre;
+    cout << "Hola mundo desde C++ (Version 2)" << endl;
+    cout << "¿Cuál es tu nombre? ";
+    cin >> nombre;
+    cout << "Encantado de conocerte, " << nombre << "!" << endl;
     return 0;
 }
